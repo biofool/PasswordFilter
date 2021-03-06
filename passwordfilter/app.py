c352bec5dec3a3c8061859d6b2cb5d87a5527519
Kenneth Kron <1031599+biofool@users.noreply.github.com>
2021-03-06 11:11:07 -0800

    Cleanup

--- passwordfilter/app.py
+++ passwordfilter/app.py
@@ -1,19 +1,19 @@
-# Passwords from
-# https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
+# user_pws from
+# https://raw.githubuseruser_pw.com/danielmiessler/SecLists/master/user_pws/Common-Credentials/10-million-user_pw-list-top-1000000.txt
 """
-export FLASK_APP=passwordbetterer.py
+export FLASK_APP=user_pwbetterer.py
 export FLASK_ENV=development
 flask run
 """
-# def passworddecensy(password):
+# def user_pwdecensy(user_pw):
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 import random
 import string
 
-MinPasswordLen = 14
-#  Modify banned password set to ignore passwords shorter than MinPasswordLen
+Minuser_pwLen = 14
+#  Modify banned user_pw set to ignore user_pws shorter than Minuser_pwLen
 compromised = ()
 
 
@@ -35,10 +35,10 @@ def init(custombadwordfile):
     if custombadwordfile:
         badwordfile = custombadwordfile
     else:
-        badwordfile = '10-million-password-list-top-1000000.txt'
+        badwordfile = '10-million-user_pw-list-top-1000000.txt'
     with open(badwordfile) as compromised10K:
         compromised = set(map(str.rstrip, compromised10K))
-    return '{} compromised passwords injested', len(compromised)
+    return '{} compromised user_pws injested', len(compromised)
     # Validate
 
 
@@ -57,7 +57,7 @@ def get_db_connection():
 def get_post(post_id):
     global post
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+    post = conn.execute('SELECT * FROM users WHERE user_id = ?',
                         (post_id,)).fetchone()
     conn.close()
     if post is None:
@@ -68,15 +68,15 @@ def get_post(post_id):
 @app.route('/')
 def index():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
+    users = conn.execute('SELECT * FROM users').fetchall()
     conn.close()
     if len(compromised) == 0:
-        init("10-million-password-list-top-1000000.txt")
+        init("10-million-user_pw-list-top-1000000.txt")
 
     return render_template('index.html')
 
 
-# ('10-million-password-list-top-1000000.txt')
+# ('10-million-user_pw-list-top-1000000.txt')
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
@@ -86,26 +86,26 @@ def post(post_id):
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'GET':
-        msg = 'Your suggested password is ', get_random_string(15)
+        msg = 'Your suggested user_pw is ', get_random_string(15)
         flash(msg)
 
     if request.method == 'POST':
-        title = request.form ['user']
-        content = request.form ['password']
+        user = request.form ['user']
+        user_pw = request.form ['user_pw']
 
-        if not title:
+        if not user:
             flash('User is required!')
-        elif len(content) < MinPasswordLen:
-            flash("Password length must exceed 8 characters")
-        elif len(content) < MinPasswordLen:
-            flash("Password length must exceed 8 characters")
+        elif len(user_pw) < Minuser_pwLen:
+            flash("user_pw length must exceed 8 characters")
+        elif len(user_pw) < Minuser_pwLen:
+            flash("user_pw length must exceed 8 characters")
 
-        elif content in compromised:
-            flash("That password is in the list of most commonly used passwords and is banned")
+        elif user_pw in compromised:
+            flash("That user_pw is in the list of most commonly used user_pws and is banned")
         else:
             conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+            conn.execute('INSERT INTO users (user_id, user_pw) VALUES (?, ?)',
+                         (user, user_pw))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
@@ -115,20 +115,20 @@ def create():
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
     post = get_post(id)
-    msg = 'Your suggested password is', get_random_string(15)
+    msg = 'Your suggested user_pw is', get_random_string(15)
     flash(msg)
 
     if request.method == 'POST':
-        title = request.form ['title']
-        content = request.form ['content']
+        user_id = request.form ['user_id']
+        user_pw = request.form ['user_pw']
 
-        if not title:
-            flash('Title is required!')
+        if not user_id:
+            flash('user_id is required!')
         else:
             conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
+            conn.execute('UPDATE users SET user_id = ?, user_pw = ?'
                          ' WHERE id = ?',
-                         (title, content, id))
+                         (user_id, user_pw, id))
             conn.commit()
             conn.close()
             return redirect(url_for('index'))
